28f81f7fef0cdef95826dd3113bdb28e0d350f60
dani-x3 <daniela.rinscheid@tum.de>
2017-11-08 13:01:09 +0100

    Bugfix

--- src/HelloUser.java
+++ src/HelloUser.java
@@ -1,4 +1,11 @@
 
 public class HelloUser {
-
+	private String userName; 
+	
+	public HelloUser(String pUserName) {
+		userName=pUserName;
+	}
+	public void greetUser() {
+		System.out.println("Hello" + this.userName + "!")
+	}
 }
